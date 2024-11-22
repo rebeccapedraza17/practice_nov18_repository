f8b0bf27b49bc2013e356cc5e6272de74b8943ec
rebeccapedraza17 <rebepedraza17@gmail.com>
2024-11-22 14:15:10 -0700

    Add files via upload

---
+++ practice_nov22_.2.py
@@ -0,0 +1,12 @@
+# Rebecca Pedraza
+# Nov 22
+
+students = ["gracie","joao","hayden","gregon","matthew","jackson"]
+
+grades = ["3.5","4.0","2.75","100","3.75","3.0"]
+
+students[4]
+grades[4]
+
+students.append("ronan")
+grades.append("20")
